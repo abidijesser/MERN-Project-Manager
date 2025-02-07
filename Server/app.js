8cc776e8f011c201bb2eff0f85c884a9d5c88972
abidijesser <jasserabidi00@gmail.com>
2025-02-07 19:19:49 +0100

    added both client and server environement

---
+++ Server/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const db = require('./config/db.json');
+const indexRoutes = require('./routes/index');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/users', indexRoutes);
+
+// Connect to MongoDB
+mongoose.connect(db.url)
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.log('Error:', err));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
