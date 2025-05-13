640738807635ffac1faebaf0132189d199998e57
KamilChak <odragon707@gmail.com>
2025-05-13 20:22:43 +0100

    .

--- Server/app.js
+++ Server/app.js
@@ -435,7 +435,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, '192.168.33.10', () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
